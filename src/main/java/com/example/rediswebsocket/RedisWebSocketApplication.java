0628f35f613111dd18a32fcd5abad8559d85e486
qkrckstjq <cksk49@naver.com>
2024-04-21 16:05:43 +0900

    init commit

---
+++ src/main/java/com/example/rediswebsocket/RedisWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.rediswebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rediswebsocket/RedisWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rediswebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
